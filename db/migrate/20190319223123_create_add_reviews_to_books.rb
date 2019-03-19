55088d4d46cd13f853e5db839f7438bff29403f2
Deonte Cooper <deonte.cooper12@gmail.com>
2019-03-19 16:36:33 -0600

    Added new Book foreign_key
    Co-authored-by: Earl Stephens <34906415+earl-stephens@users.noreply.github.com>

--- db/migrate/20190319223123_create_add_reviews_to_books.rb
+++
@@ -1,7 +0,0 @@
-class CreateAddReviewsToBooks < ActiveRecord::Migration[5.1]
-  def change
-    create_table :add_reviews_to_books do |t|
-      t.references :book, foreign_key: true
-    end
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190319223123) do
+ActiveRecord::Schema.define(version: 20190319223544) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "add_reviews_to_books", force: :cascade do |t|
-    t.bigint "book_id"
-    t.index ["book_id"], name: "index_add_reviews_to_books_on_book_id"
-  end
 
   create_table "author_books", force: :cascade do |t|
     t.bigint "book_id"
@@ -51,9 +46,11 @@ ActiveRecord::Schema.define(version: 20190319223123) do
     t.string "user_name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "book_id"
+    t.index ["book_id"], name: "index_reviews_on_book_id"
   end
 
-  add_foreign_key "add_reviews_to_books", "books"
   add_foreign_key "author_books", "authors"
   add_foreign_key "author_books", "books"
+  add_foreign_key "reviews", "books"
 end
