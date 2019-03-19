69c40bcf82519f475e0411d372bf4731e9795a6c
Deonte Cooper <deonte.cooper12@gmail.com>
2019-03-18 18:34:52 -0600

    Added seed file and factories
    Co-authored-by: Earl Stephens <34906415+earl-stephens@users.noreply.github.com>

---
+++ spec/factories/books.rb
@@ -0,0 +1,8 @@
+FactoryBot.define do
+  factory :book do
+    sequence(:title) {|n| "Title #{n}"}
+    pages { 305 }
+    year_pub { 2006 }
+    image {"https://previews.123rf.com/images/wektorygrafika/wektorygrafika1601/wektorygrafika160100003/50246517-open-book-vector-clipart-silhouette-symbol-icon-design-illustration-isolated-on-white-background-.jpg"}
+  end
+end
