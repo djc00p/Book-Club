81880b50aa229dce0e958d863a0ecb38695c4ecf
earl-stephens <stephensearl@hotmail.com>
2019-03-23 23:51:20 -0600

    turned book titles into hyperlinks to the book show page

--- spec/features/book/show_spec.rb
+++ spec/features/book/show_spec.rb
@@ -1,15 +1,10 @@
 require 'rails_helper'
 
-# When I visit a book's show page,
-# I see an area on the page for statistics about reviews:
-# - the top three reviews for this book (title, rating and user only)
-# - the bottom three reviews for this book  (title, rating and user only)
-# - the overall average rating of all reviews for this book
-
 describe "on the book show page" do
   context "shows book statistics" do
     before :each do
       @book_1 = create(:book)
+      @book_2 = create(:book)
       @review_1 = create(:review, book_id: @book_1.id)
       @review_2 = create(:review, rating: 4, user_name: "John", book_id: @book_1.id)
       @review_3 = create(:review, rating: 5, user_name: "Joe", book_id: @book_1.id)
@@ -62,23 +57,6 @@ describe "on the book show page" do
       end
     end
 
-    # As a Visitor,
-# When I visit a book's show page
-# I see a link to add a new review for this book.
-# When I click on this link, I am taken to a new review path.
-# On this new page, I see a form where I can enter:
-# - a review title
-# - a username as a string
-# - a numeric rating that can only be a number from 1 to 5
-# - some text for the review itself
-# When the form is submitted, I should return to that book's
-# show page and I should see my review text.
-#
-# Users are created if they do not already exist.
-# User names should be converted to Title Case before saving.
-# User names should be unique within the system.
-# If user existed in the database, this review is associated
-# with that user.
     it "should let a user create a new review" do
       visit book_path(@book_1)
 
