964af428c2e9d2bccffc4a5091772ed0dafd370d
earl-stephens <stephensearl@hotmail.com>
2019-03-24 14:30:17 -0600

    finished user story 18 closes #8

--- spec/features/users/show_spec.rb
+++ spec/features/users/show_spec.rb
@@ -1,37 +1,26 @@
 require 'rails_helper'
 
-# When I click on a user's name for any book review
-# I am taken to a show page for that user.
-# I should see all reviews that this
-# user has written.
-# Each review shows:
-# - the title of the review
-# - the description of the review
-# - the rating of the review
-# - the title of the book
-# - the thumbnail image for the book
-# - the date the review was written
-
 describe "on the user show page" do
   before :each do
     @book_1 = create(:book)
     @book_2 = create(:book)
+    @book_3 = create(:book)
+    @book_4 = create(:book)
     @review_1 = create(:review, book_id: @book_1.id)
     @review_2 = create(:review, rating: 4, book_id: @book_2.id)
     @review_3 = create(:review, rating: 5, user_name: "Joe", book_id: @book_1.id)
+    @review_4 = create(:review, rating: 2, user_name: "Joe", book_id: @book_2.id)
+    @review_5 = create(:review, rating: 1, user_name: "Jeff", book_id: @book_4.id)
+    @review_6 = create(:review, rating: 5, user_name: "Joycerak", book_id: @book_3.id)
   end
 
   it "shows all reviews for that user" do
 
     visit book_path(@book_1)
-    # save_and_open_page
 
     within ".book_show_info" do
-      # binding.pry
       click_link "#{@review_1.user_name}"
     end
-      # save_and_open_page
-      # binding.pry
 
     expect(current_path).to eq("/user/#{@review_1.user_name}")
     expect(page).to have_content("Review Title: #{@review_1.title}")
@@ -46,5 +35,21 @@ describe "on the user show page" do
     expect(page).to have_content("Date of Review: #{@review_2.created_at}")
     expect(page).to_not have_content("Review Title: #{@review_3.title}")
   end
+
+  it "delete a review from a user with multiple reviews" do
+    visit "/user/#{@review_3.user_name}"
+
+    click_link "Delete Review: #{@review_3.title}"
+
+    expect(page).to_not have_content("Review Title: #{@review_3.title}")
+    expect(page).to have_content("Review Title: #{@review_4.title}")
+  end
+
+  it "delete a review from a user with a single review" do
+    visit "/user/#{@review_5.user_name}"
+
+    click_link "Delete Review: #{@review_5.title}"
+
+    expect(current_path).to eq(books_path)
+  end
 end
-# end
