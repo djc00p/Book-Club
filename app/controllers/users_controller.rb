0914033e6ab5a3a1e94677f5e5c992b9783423ac
Deonte Cooper <deonte.cooper12@gmail.com>
2019-05-27 13:01:22 -0600

    Added travis ci

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,9 @@
 class UsersController < ApplicationController
 
   def show
-    # binding.pry
     @user_name = params[:id]
     case params[:sort]
     when "descend"
-      # binding.pry
       @user_review = Review.where(user_name: params[:id]).order(created_at: :desc)
     when "ascend"
       @user_review = Review.where(user_name: params[:id]).order(:created_at)
